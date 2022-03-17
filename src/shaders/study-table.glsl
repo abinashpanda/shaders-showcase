8049b84c946ccef5360ed3ad785130cda618ec85
Abinash Panda <abinash@prodios.com>
2022-03-18 01:44:38 +0530

    feat(study-table): added windows and shadows

--- src/shaders/study-table.glsl
+++ src/shaders/study-table.glsl
@@ -7,7 +7,7 @@ It should be only used as a template for creating new ray-marcher.
 #define MAX_STEPS 100
 #define MAX_DIST 100.
 #define SURF_DIST .001
-#define ZOOM 0.5
+#define ZOOM 1.5
 #define TAU 6.2831
 
 precision mediump float;
@@ -67,8 +67,8 @@ float sdCappedCone(vec3 point, float h, float r1, float r2) {
 
 // scene specific sdf
 
-float sdTableLeg(vec3 point, vec3 pos) {
-  return sdBox(point + pos, vec3(0.03, 0.5, 0.03)) - 0.02;
+float sdTableLeg(vec3 point) {
+  return sdBox(point, vec3(0.03, 0.5, 0.03)) - 0.02;
 }
 
 // ray march functions
@@ -79,6 +79,11 @@ float getDistance(vec3 point) {
 
   float floor = sdBox(point, vec3(2.0, 0.1, 2.0));
   float backWall = sdBox(point - vec3(0.0, 1.9, -2.0), vec3(2.0, 2.0, 0.1));
+
+  vec3 wPoint = point - vec3(0.0, 2.3, -2.0);
+  wPoint = vec3(abs(wPoint.x + 0.8) - 0.32, abs(wPoint.y) - 0.332, wPoint.z);
+  float window = sdBox(wPoint, vec3(0.3, 0.3, 0.2));
+  backWall = max(backWall, -window);
   float room = min(floor, backWall);
   float d = room;
 
@@ -86,13 +91,9 @@ float getDistance(vec3 point) {
   point -= vec3(0.7, 1.12, 0.9);
 
   float tableTop = sdBox(point, vec3(1, 0.01, 0.5)) - 0.05;
-  float tableLeg1 = sdTableLeg(point, vec3(-.8, 0.5, -0.4));
-  float tableLeg2 = sdTableLeg(point, vec3(.8, 0.5, -0.4));
-  float tableLeg3 = sdTableLeg(point, vec3(.8, 0.5, 0.4));
-  float tableLeg4 = sdTableLeg(point, vec3(-.8, 0.5, 0.4));
-  float tableLeg = min(tableLeg1, tableLeg2);
-  tableLeg = min(tableLeg, tableLeg3);
-  tableLeg = min(tableLeg, tableLeg4);
+  // make the values absolute to run in a single loop and make it symmetric
+  vec3 tlPoint = vec3(abs(point.x) - 0.8, point.y + 0.5, abs(point.z) - 0.4);
+  float tableLeg = sdTableLeg(tlPoint);
   float table = min(tableTop, tableLeg);
 
   d = min(d, table);
@@ -109,15 +110,20 @@ float getDistance(vec3 point) {
   point += vec3(0.7, 0.08, 0.2);
 
   // table offset add
-  point += vec3(0.7, 1.12, 0.9);
+  point += vec3(0.7, 0.7, 0.9);
 
   // sofa offset subtract
-  point -= vec3(0.0, 0.3, -1.35);
-  float sofaBase = sdBox(point, vec3(1.25, 0.2, 0.5));
-  float sofa = sofaBase;
+  point -= vec3(0.0, 0.29, -1.2);
+  float sofaBase = sdBox(point - vec3(0.0, -0.3, 0.0), vec3(1.25, 0.2, 0.5));
+  vec3 ssPoint = vec3(abs(point.x) - 1.25, point.y + 0.1, point.z);
+  float sofaSide = sdBox(ssPoint, vec3(0.07, 0.4, 0.5));
+  float sofaBack = sdBox(point + vec3(0.0, 0.1, 0.5), vec3(1.32, 0.4, 0.07));
+  float sofa = min(sofaBase, sofaSide);
+  sofa = min(sofa, sofaBack);
+  vec3 slPoint = vec3(abs(point.x) - 1.2, point.y + 0.55, abs(point.z) - 0.35);
+  float sofaLeg = sdCappedCone(slPoint, 0.05, 0.02, 0.05);
   d = min(d, sofa);
-
-  // float d = lamp;
+  d = min(d, sofaLeg);
 
   return d;
 }
@@ -156,20 +162,25 @@ void main() {
   vec2 pos = (uv - vec2(0.5)) * vec2(uAspectRatio, 1);
   vec2 mouse = uMouse / uResolution;
 
-  vec3 rayOrigin = vec3(0, 3, -3);
+  vec3 rayOrigin = vec3(0, 5, -5);
   rayOrigin.yz *= rotate(-mouse.y * 3.14 + 1.);
   rayOrigin.xz *= rotate(-mouse.x * 6.2831);
 
-  vec3 rayDirection = getRayDirection(pos, rayOrigin, vec3(0, 0., 0), ZOOM);
+  vec3 rayDirection = getRayDirection(pos, rayOrigin, vec3(0, 1.5, 0), ZOOM);
   vec3 color = vec3(0);
+  vec3 lightPos = vec3(-10, 5, -10);
 
   float d = rayMarch(rayOrigin, rayDirection);
   if(d < MAX_DIST) {
     vec3 p = rayOrigin + rayDirection * d;
     vec3 n = getNormal(p);
-    // vec3 r = reflect(rayDirection, n);
 
-    float diffuseLighting = dot(n, normalize(vec3(1, 2, 3))) * .5 + .5;
+    float diffuseLighting = dot(n, normalize(lightPos)) * .5 + .5;
+    float rayMarchToLight = rayMarch(p + n * SURF_DIST * 2.0, normalize(lightPos - p));
+    if(rayMarchToLight < length(lightPos - p)) {
+      diffuseLighting *= 0.7;
+    }
+
     color = vec3(diffuseLighting);
   }
 
